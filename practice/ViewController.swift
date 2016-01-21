54f04fbf7043da972a701824aa34574245595662
伴貴史 <Ur.one-in-a---million@softbank.ne.jp>
2016-01-21 15:56:05 +0900

    Initial Commit

---
+++ practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practice
+//
+//  Created by 株式会社ConU on 2016/01/21.
+//  Copyright © 2016年 株式会社ConU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
