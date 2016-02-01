9d432d784eea8f83a22227257202e9ba33c78787
伴貴史 <Ur.one-in-a---million@softbank.ne.jp>
2016-02-01 21:12:03 +0900

    parse connected

--- practice/NewEventViewController.swift
+++ practice/NewEventViewController.swift
@@ -12,30 +12,37 @@ import Parse
 
 class NewEventViewController: UIViewController, LiquidFloatingActionButtonDataSource, LiquidFloatingActionButtonDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
     
+    var sectionTitleArray : NSMutableArray = NSMutableArray()
+    var sectionContentDict : NSMutableDictionary = NSMutableDictionary()
+    var arrayForBool : NSMutableArray = NSMutableArray()
    
+   
+    @IBOutlet weak var tableView: UITableView!
     
- //   @IBOutlet weak var titleField: UITextField!　木下アホや あはんあはん
+  @IBOutlet weak var titleField: UITextField!
    
     @IBOutlet weak var imageView: UIImageView!
 
-    
+  
 
     
     //追加のメンバ変数
     var eventTitle :String = ""
     var eventPhoto :UIImage! = nil
-    var eventPlace :String = ""
-    var eventDate  :String = ""
+    var eventPlace :[String] = []
+    var eventDate  :[String] = []
     
     //保存用メンバ変数
     
     var saveEventTitle :String!
     var saveEventPhoto :UIImage? = nil
     var uploadEventPhoto :PFFile!
-    var saveEventPlace :String!
-    var saveEventDate :String!
+    var saveEventPlace :[String] = []
+    var saveEventDate :[String] = []
     
     
+    
+
     
     
     
@@ -44,9 +51,26 @@ class NewEventViewController: UIViewController, LiquidFloatingActionButtonDataSo
     
     @IBOutlet weak var closeButton: UIButton!
     
+    
+    
+    
     override func viewDidLoad() {
         
-//        titleField.delegate = self
+     titleField.delegate = self
+        
+        
+        arrayForBool = ["0","0"]
+        sectionTitleArray = ["PLACE","DATE & TIME",]
+        let countryListA : NSArray = titleList
+        var string1 = sectionTitleArray .objectAtIndex(0) as? String
+        [sectionContentDict .setValue(countryListA, forKey:string1! )]
+        let countryListB : NSArray = dateList
+        string1 = sectionTitleArray .objectAtIndex(1) as? String
+        [sectionContentDict .setValue(countryListB, forKey:string1! )]
+        
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+
     
         
         self.view.bringSubviewToFront(imageView)
@@ -61,6 +85,9 @@ class NewEventViewController: UIViewController, LiquidFloatingActionButtonDataSo
             return floatingActionButton
             
         
+            
+            
+            
         }
         
         
@@ -77,7 +104,7 @@ class NewEventViewController: UIViewController, LiquidFloatingActionButtonDataSo
         
        //フローティングの配置
         
-        let floatingFrame2 = CGRect(x: 0, y: 80, width: 56, height: 56)
+        let floatingFrame2 = CGRect(x: 0, y: 230, width: 50, height: 50)
         let topLeftButton = createButton(floatingFrame2, .Right)
         
                 self.view.addSubview(topLeftButton)
@@ -86,6 +113,11 @@ class NewEventViewController: UIViewController, LiquidFloatingActionButtonDataSo
     }
     
     //終了ボタン
+    
+    
+    
+    
+    
     
     @IBAction func closeAction(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
@@ -112,6 +144,106 @@ class NewEventViewController: UIViewController, LiquidFloatingActionButtonDataSo
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return sectionTitleArray.count
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        if(arrayForBool .objectAtIndex(section).boolValue == true)
+        {
+            let tps = sectionTitleArray.objectAtIndex(section) as! String
+            let count1 = (sectionContentDict.valueForKey(tps)) as! NSArray
+            return count1.count
+        }
+        return 0;
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "ABC"
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        return 50
+    }
+    
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if(arrayForBool .objectAtIndex(indexPath.section).boolValue == true){
+            return 60
+        }
+        
+        return 2;
+    }
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let headerView = UIView(frame: CGRectMake(0, 0, tableView.frame.size.width, 55))
+        headerView.backgroundColor = UIColor.flatPurpleColorDark()
+        headerView.tag = section
+        
+        
+        let headerString = UILabel(frame: CGRect(x: 5, y: 10, width: tableView.frame.size.width-10, height: 40)) as UILabel
+        headerString.text = sectionTitleArray.objectAtIndex(section) as? String
+        headerString.font = UIFont(name: "Arcon", size: 30)
+        headerString.textAlignment = .Center
+        headerString.textColor = UIColor.whiteColor()
+        headerView .addSubview(headerString)
+        
+        let headerTapped = UITapGestureRecognizer (target: self, action:"sectionHeaderTapped:")
+        headerView .addGestureRecognizer(headerTapped)
+        
+        return headerView
+    }
+    
+    func sectionHeaderTapped(recognizer: UITapGestureRecognizer) {
+        print("Tapping working")
+        print(recognizer.view?.tag)
+        
+        let indexPath : NSIndexPath = NSIndexPath(forRow: 0, inSection:(recognizer.view?.tag as Int!)!)
+        if (indexPath.row == 0) {
+            
+            var collapsed = arrayForBool .objectAtIndex(indexPath.section).boolValue
+            collapsed       = !collapsed;
+            
+            arrayForBool .replaceObjectAtIndex(indexPath.section, withObject: collapsed)
+            //reload specific section animated
+            let range = NSMakeRange(indexPath.section, 1)
+            let sectionToReload = NSIndexSet(indexesInRange: range)
+            self.tableView .reloadSections(sectionToReload, withRowAnimation:UITableViewRowAnimation.Fade)
+        }
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        
+        let cellIdentifier = "Cell"
+        let cell: UITableViewCell! = self.tableView.dequeueReusableCellWithIdentifier(cellIdentifier)
+        
+        let manyCells : Bool = arrayForBool .objectAtIndex(indexPath.section).boolValue
+        
+        if (!manyCells) {
+            //  cell.textLabel.text = @"click to enlarge";
+        }
+        else{
+            let content = sectionContentDict .valueForKey(sectionTitleArray.objectAtIndex(indexPath.section) as! String) as! NSArray
+            cell.textLabel?.text = content .objectAtIndex(indexPath.row) as? String
+            cell.backgroundColor = UIColor .flatPurpleColor()
+            cell.textLabel?.textColor = UIColor.whiteColor()
+            cell.layer.frame.size = CGSize(width: tableView.frame.width, height: 60)
+        }
+        
+        return cell
+    }
+    
+
+  
+    
     func numberOfCells(liquidFloatingActionButton: LiquidFloatingActionButton) -> Int {
         return cells.count
     }
@@ -123,10 +255,20 @@ class NewEventViewController: UIViewController, LiquidFloatingActionButtonDataSo
     func liquidFloatingActionButton(liquidFloatingActionButton: LiquidFloatingActionButton, didSelectItemAtIndex index: Int) {
         print("did Tapped! \(index)")
         switch index{
+        case 0:
+            let storyboard:UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            
+            let next:UIViewController = storyboard.instantiateViewControllerWithIdentifier("Date") as UIViewController
+            
+            next.modalTransitionStyle = UIModalTransitionStyle.CrossDissolve
+            
+            self.presentViewController(next, animated: true, completion: nil)
+        
+
         case 1:
             let storyboard:UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
             
-            let next:UIViewController = storyboard.instantiateViewControllerWithIdentifier("Time") as UIViewController
+            let next:UIViewController = storyboard.instantiateViewControllerWithIdentifier("Place") as UIViewController
             
             next.modalTransitionStyle = UIModalTransitionStyle.CrossDissolve
             
@@ -162,15 +304,17 @@ class NewEventViewController: UIViewController, LiquidFloatingActionButtonDataSo
         self.saveEventPhoto = self.imageView.image
         let imageData :NSData = UIImagePNGRepresentation(self.saveEventPhoto!)!
         self.uploadEventPhoto = PFFile(name: "event.png", data: imageData)!
-        
-//        self.saveEventTitle = self.titleField.text
+        self.saveEventTitle = self.titleField.text
+        self.saveEventDate = dateList
+        self.saveEventPlace = titleList
         
         let savingObjects = PFObject(className: "Event")
         
-       // savingObjects["title"] = self.saveEventTitle
+       savingObjects["title"] = self.saveEventTitle
         savingObjects["photo"] = self.uploadEventPhoto
         savingObjects["user"] = PFUser.currentUser()!.username
-     //   savingObjects["date"] = self.saveEventDate
+     savingObjects["date"] = self.saveEventDate
+        savingObjects["place"] = self.saveEventPlace
         
         savingObjects.saveInBackgroundWithBlock{ (success: Bool, error: NSError?) -> Void in
             
@@ -187,8 +331,8 @@ class NewEventViewController: UIViewController, LiquidFloatingActionButtonDataSo
         
     }
     
-    
     }
+
 
     
     
